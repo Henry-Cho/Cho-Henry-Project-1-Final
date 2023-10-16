028554f0f57c708de56c000a72cd0fa4aad3e46d
Henry Cho <hyungseok5926@gmail.com>
2023-10-16 04:27:15 -0600

    Initial Commit

---
+++ Cho Henry Project 1/Cho_Henry_Project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Cho_Henry_Project_1App.swift
+//  Cho Henry Project 1
+//
+//  Created by Henry Cho on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Cho_Henry_Project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cho Henry Project 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cho Henry Project 1
+//
+//  Created by Henry Cho on 10/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
